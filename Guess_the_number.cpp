4cc6fbd33bbd63d8a8fbf0b403587fc714cf1952
xQuiets <danil_l_2007@mail.ru>
2026-06-24 11:59:24 +0400

    Initial commit: базовая версия игры Угадай число

---
+++ Guess_the_number.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+    setlocale(LC_ALL, "");
+    srand(clock());
+    string username;
+    int num = rand() % 998 + 1;
+    cout << "Программа: угадай число.\n";
+    cout << "Компьютер загадал число от 1 до 999. \n";
+    cout << "Угадай его за минимальное число попыток. \n";
+    int usernum;
+    int count = 0;
+    while (1) {
+        do
+        {
+            cout << "Введите число: ";
+            cin >> usernum;
+            if (usernum < 1 || usernum > 999) {
+                cout << "Число должно лежать в диапазоне от 1 до 999. Повторите ввод. \n";
+            }
+        } while (usernum < 1 || usernum > 999);
+        count++;
+        if (usernum > num) {
+            cout << "Перелёт...\n";
+        }
+        else if (usernum < num) {
+            cout << "Недолёт...\n";
+        }
+        else {
+            cout << "Вы угадали число " << num <<
+                " за " << count << " попыток.\n";
+            break;
+        }
+    }
+}
